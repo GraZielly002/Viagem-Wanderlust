33fbd9d7020b8c1b170fe68dabac8d63e15e42f6
Grazielly <graziellycosta396@gmail.com>
2023-10-15 19:39:27 -0300

    arrumando

--- cadastro.js
+++ cadastro.js
@@ -2,5 +2,18 @@ function mostrarOcultarSenha(){
     var senha=document.getElementById("senha");
     if(senha.type=="password"){
         senha.type="text";
+    } else if (senha.type=="text"){
+        senha.type="password";
+    }
+
+}
+function enviar(){
+    var dados={nome:document.getElementById('nome').value,
+                user:document.getElementById('usuario').value,
+                email:document.getElementById('email').value,
+                senha:document.getElementById("senha").value};
+    if(dados){
+        window.alert('Dados cadastrados');
+        window.alert(`Prazer em te conhecer ${dados.user}`);
     }
 }
